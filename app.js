dae8c0313a289fdbade68af9f6b0aa5fe4635807
Nick Remlinger <nwremlinger@gmail.com>
2014-09-04 16:43:27 +0200

    created directives

--- app.js
+++ app.js
@@ -23,6 +23,25 @@
     };
   });
 
+  app.directive("productTabs", function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-tabs.html',
+      controller: function() {
+        this.tab =1;
+
+        this.isSet = function(checkTab) {
+          return this.tab === checkTab;
+        };
+
+        this.setTab = function(setTab) {
+          this.tab = setTab;
+        };
+      },
+      controllerAs: 'tab'
+    };
+  })
+
    app.controller('ReviewController', function() {
     this.review = {};
     
@@ -30,6 +49,28 @@
       this.review.createdOn = Date.now();
       product.reviews.push(this.review);
       this.review = {};
+    };
+  });
+
+
+   app.directive("productDescription", function() {
+    return {
+      restrict: 'E',
+        templateUrl: 'product-description.html'
+    };
+   });
+
+   app.directive("productReviews", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-reviews.html"
+    };
+  });
+
+  app.directive("productSpecs", function() {
+    return {
+      restrict: 'A',
+      templateUrl: 'product-specs.html'
     };
   });
 
