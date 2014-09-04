2be4b166a1daf7704b97416debc8b9cb2987c5d1
Nick Remlinger <nwremlinger@gmail.com>
2014-09-04 16:12:45 +0200

    added form validation

--- app.js
+++ app.js
@@ -25,10 +25,14 @@
 
    app.controller('ReviewController', function() {
     this.review = {};
+    
     this.addReview = function(product){
-            product.reviews.push(this.review);
+      this.review.createdOn = Date.now();
+      product.reviews.push(this.review);
       this.review = {};
     };
+  });
+
 
   var gems = [
       {
