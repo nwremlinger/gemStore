ee2da785d251e9af810d03808956b814fb75b79f
Nick Remlinger <nwremlinger@gmail.com>
2014-09-04 15:59:53 +0200

    added review controller

--- app.js
+++ app.js
@@ -23,6 +23,13 @@
     };
   });
 
+   app.controller('ReviewController', function() {
+    this.review = {};
+    this.addReview = function(product){
+            product.reviews.push(this.review);
+      this.review = {};
+    };
+
   var gems = [
       {
         name: 'Azurite',
