d15a7b07b2ff2d91f00d4dff4e5b0fbd2e5554c3
Nick Remlinger <nwremlinger@gmail.com>
2014-09-04 16:48:16 +0200

    refactor product gallery

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 (function() {
   var app = angular.module('gemStore', []);
 
+  app.directive('productGallery', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-gallery.html',
+      controller: function() {
+        this.current = 0;
+        this.setCurrent = function(imageNumber) {
+          this.current = imageNumber || 0;
+        };
+      },
+      controllerAs: 'gallery'
+    };
+  });
+
   app.controller('GalleryController', function(){
     this.current = 0;
     this.setCurrent = function(newGallery){
